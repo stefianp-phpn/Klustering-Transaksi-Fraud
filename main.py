d1e5fb67fe72c25c07cd76e4ef578964d3dba2fc
stefianp-phpn <stefyankuroumang@gmail.com>
2025-07-03 00:24:58 +0700

    Update main.py

--- main.py
+++ main.py
@@ -37,14 +37,14 @@ if uploaded_file:
     try:
         # Step 1: Encoding
         encoded_array = encoder.transform(df[cat_cols])
-        #st.write("📌 Bentuk hasil encoded_array:", encoded_array.shape)        
+        st.write("📌 Bentuk hasil encoded_array:", encoded_array.shape)        
         temp_column = encoder.get_feature_names_out(cat_cols).tolist()
         #st.write("Tipe data encoded_array:", type(encoded_array))        
-        #st.write("📌 Nama kolom hasil encoding saat ini:", temp_column)
+        st.write("📌 Nama kolom hasil encoding saat ini:", temp_column)
         #st.write("📌 Nama kolom hasil encoding training:", encoded_columns.shape)        
 
-        encoded_df = pd.DataFrame(encoded_array, columns=temp_column)
-        encoded_df = encoded_df.reindex(columns=encoded_columns, fill_value=0)
+        #encoded_df = pd.DataFrame(encoded_array, columns=temp_column)
+        #encoded_df = encoded_df.reindex(columns=encoded_columns, fill_value=0)
                 
         #missing = set(encoded_columns) - set(encoded_df.columns)
         #extra = set(encoded_df.columns) - set(encoded_columns)
