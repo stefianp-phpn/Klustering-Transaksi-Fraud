0dc447b7b59bdb92a1357003d7c6fc500c9b3b62
stefianp-phpn <stefyankuroumang@gmail.com>
2025-07-02 23:36:41 +0700

    main.py

--- main.py
+++ main.py
@@ -43,10 +43,10 @@ if uploaded_file:
         #encoded_df = pd.DataFrame(encoded_array, columns=temp_column)
         #encoded_df = encoded_df.reindex(columns=encoded_columns, fill_value=0)
                 
-        missing = set(encoded_columns) - set(encoded_df.columns)
-        extra = set(encoded_df.columns) - set(encoded_columns)
-        st.write("❗ Kolom hilang dari data saat ini:", missing)
-        st.write("❗ Kolom tambahan yang tidak dikenali:", extra)   
+        #missing = set(encoded_columns) - set(encoded_df.columns)
+        #extra = set(encoded_df.columns) - set(encoded_columns)
+        #st.write("❗ Kolom hilang dari data saat ini:", missing)
+        #st.write("❗ Kolom tambahan yang tidak dikenali:", extra)   
                 
         # Step 2: Scaling
         scaled_array = scaler.transform(df[num_cols])
