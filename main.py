b4be0d1d9907fcca4149cb81898e59a6ed7f816b
stefianp-phpn <stefyankuroumang@gmail.com>
2025-07-02 23:35:54 +0700

    main.py

--- main.py
+++ main.py
@@ -37,9 +37,11 @@ if uploaded_file:
     try:
         # Step 1: Encoding
         encoded_array = encoder.transform(df[cat_cols])
-        temp_column = encoder.get_feature_names_out(cat_cols)     
-        encoded_df = pd.DataFrame(encoded_array, columns=temp_column)
-        encoded_df = encoded_df.reindex(columns=encoded_columns, fill_value=0)
+        print(encoded_array)        
+        temp_column = encoder.get_feature_names_out(cat_cols)
+        print(temp_column)        
+        #encoded_df = pd.DataFrame(encoded_array, columns=temp_column)
+        #encoded_df = encoded_df.reindex(columns=encoded_columns, fill_value=0)
                 
         missing = set(encoded_columns) - set(encoded_df.columns)
         extra = set(encoded_df.columns) - set(encoded_columns)
